971f6037b30148a089918000e7764069026e2b53
Jessica Huynh-Westfall <jessicawestfall@gmail.com>
2024-09-25 07:32:09 -0700

    airline dataset

--- Lec07/lec07_class.sh
+++ Lec07/lec07_class.sh
@@ -21,7 +21,7 @@
 #then 
 #    echo "This is the first command"
 #    echo "this is the second command"
-#    ls -a /Users/$testuser/.b*
+#    ls -a /mnt/scratch/$testuser/.b*
 #fi
 
 ### if-then-else ###
@@ -30,7 +30,7 @@
 #then
 #    echo "This is the first command"
 #    echo "this is the second command"
-#    ls -a /Users/$testuser/.b*
+#    ls -a /mnt/scratch/$testuser/.b*
 #else
 #    echo "The user $testuser does not exist on this system"
 #fi
@@ -40,7 +40,7 @@
 #if grep $testuser /etc/passwd
 #then
 #    echo "The user $testuser exists on this system."
-#elif ls -d /home/$testuser
+#elif ls -d /mnt/scratch/$testuser
 #then
 #    echo "The user $testuser does not exist on this system."
 #    echo "However, $testuser has a directory."
@@ -48,3 +48,12 @@
 #    echo "The user $testuser does not exist on this system"
 #    echo "And, $testuser does not have a directory."
 #fi
+
+### If conditions ###
+dollar=$2
+if [ $dollar -lt 5 ]; then
+    sum=$(($dollar+10))
+    echo "You had \$5 before, now you have $sum dollars"
+else
+    echo "I don't want to calcuate your money"
+fi

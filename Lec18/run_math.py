8bd3de57180105989f2d5d000c5f66a3774e156b
Jessica Huynh-Westfall <jessicawestfall@gmail.com>
2024-11-04 09:32:01 -0800

    Unit testing materials

---
+++ Lec18/run_math.py
@@ -0,0 +1,9 @@
+from math_functions import add, subtract
+
+# Use the imported functions
+result_add = add(10, 5)
+result_subtract = subtract(10, 5)
+
+print("Addition Result:", result_add)         # Output: 15
+print("Subtraction Result:", result_subtract) # Output: 5
+
